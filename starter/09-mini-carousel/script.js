63ab33ea31151ff31fd7d306a279d8ddb95033f2
seda-karadeniz <sedayo98@gmail.com>
2020-11-26 18:17:10 +0100

    completed exercise

--- starter/09-mini-carousel/script.js
+++ starter/09-mini-carousel/script.js
@@ -14,3 +14,28 @@ AIDE : voici les étapes :
 5. En fin de longueur du tableau, on redémarre au début de l'index
 */
 
+const gallery = ["./img/bell.svg","./img/clock.svg","./img/compass.svg","./img/kiss.svg"];
+const e_image = document.querySelector('img');
+let i = 1;
+
+function myFunction(){
+    e_image.src = gallery[i];
+    i++;
+    if (i >= gallery.length) {
+        i = 0;
+    }
+}
+/* document.querySelector('#next').addEventListener('click', myFunction);
+document.querySelector('img').addEventListener('click', myFunction); */
+
+/* avec boucle forof */
+const a_nexts = document.querySelectorAll('#next, img');
+/* initialiser un tableau avec plusieur selecteur (pour que sa fonctionnent sur les img et sur le boutton) */
+
+for (const e_next of a_nexts) {
+    e_next.addEventListener('click', myFunction);
+}
+/* for of pr parcourir plusieur balise sur le quel on veux traiter un mm evenemnt */
+
+
+
